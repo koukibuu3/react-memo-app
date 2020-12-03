ce1764be4191fe13c89c34ef7680f5784fd12d45
koukibuu3 <koukibuu3@gmail.com>
2020-12-03 19:45:51 +0900

    一覧ページがトップページになるようにルーティングを作成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import List from './list/List';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <List />
   </React.StrictMode>,
   document.getElementById('root')
 );
