a40c64df2245fafd17b788a6e3158a85baf7b443
koukibuu3 <koukibuu3@gmail.com>
2020-12-22 19:06:06 +0900

    とりあえずコンポーネントを分けてみた #19

--- src/list/List.js
+++ src/list/List.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const MEMOS = [
+  { title: 'たいとる', body: 'ぼでー' },
+  { title: 'たいとる2', body: 'ぼでー2' },
+  { title: 'たいとる3', body: 'ぼでー3' },
+];
+
+const getList = () => {
+  return MEMOS;
+};
+
 const List = () => {
-  const list = ['hoge', 'huga', 'piyo'];
+  const list = getList();
 
   return (
     <div className="List">
       <ul>
         { list.map((value) => (
-          <li key="key">
-            {value}
-            <Link to="/detail"><button type="button">to detail</button></Link>
-          </li>
+          <Item content={value} />
         )) }
       </ul>
     </div>
+  );
+};
+
+const Item = (props) => {
+  const { content } = props;
+  return (
+    <li key="key">
+      {content.title}
+      <span>
+        {content.body}
+      </span>
+      <Link to="/detail"><button type="button">to detail</button></Link>
+    </li>
   );
 };
 
