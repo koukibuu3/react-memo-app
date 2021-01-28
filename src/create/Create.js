6c5c1e2be47425cc57ddab11a948754f30c4cce3
t-rakuyama <t.rakuyama929@gmail.com>
2021-01-28 19:04:28 +0900

    react-fook-form導入

--- src/create/Create.js
+++ src/create/Create.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 import Header from '../components/Header';
 
 const Create = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data)
+
   return (
     <>
       <Header/>
       <h1>追加ページ</h1>
-      <table>
-        <tr>
-          <th>タイトル</th>
-          <td><textarea placeholder="タイトルを入力してください" /></td>
-        </tr>
-        <tr>
-          <th>本文</th>
-          <td><textarea placeholder="本文を入力してください" /></td>
-        </tr>
-      </table>
-      <Link to="/"><button type="button">戻る</button></Link>
-      <Link to="/"><button type="button">保存</button></Link>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="title" placeholder="タイトルを入力してください" ref={register({ required: true, maxLength: 20 })} />
+        {errors.title && <span>タイトルが未入力です</span>}
+        <input name="body" placeholder="本文を入力してください" ref={register({ required: true})} />
+        <input type="submit" />
+      </form>
+      <Link to='/'><button>戻る</button></Link>
     </>
   );
 };
