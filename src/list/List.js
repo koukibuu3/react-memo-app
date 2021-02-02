5d22f0cf63d0c7c56254c8131801aaaa52105936
koukibuu3 <koukibuu3@gmail.com>
2021-02-02 23:48:27 +0900

    feat: Cardのデザインを調整 #19

--- src/list/List.js
+++ src/list/List.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Grid, Card, CardContent, CardActions, Typography } from '@material-ui/core';
+import { Button, Grid, Card, CardContent, Typography } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
 import { makeStyles } from "@material-ui/core/styles";
-
-import Header from '../components/Header';
 
 const useStyles = makeStyles(() => ({
   card: {
     height: 180,
-    width: 180
+    width: 180,
   },
   createButton: {
-    margin: 'auto'
+    height: '100%',
+    width: '100%',
+  },
+  detailButton: {
+    height: '100%',
+    width: '100%',
+    '& a': {
+      textDecoration: 'none',
+      color: 'inherit',
+    }
   }
 }));
 
@@ -33,24 +41,25 @@ const List = () => {
 
   return (
     <div className="List">
-      <Header/>
       <Grid container direction="row" spacing={3}>
         <Grid item spacing={3}>
           <Card className={classes.card}>
-            <CardActions>
-              <Button className={classes.createButton} size="medium">
-                <Link to="/create">create</Link>
-              </Button>
-            </CardActions>
+            <Button className={classes.createButton}>
+              <Link to="/create"><Add fontSize="large" /></Link>
+            </Button>
           </Card>
         </Grid>
         { list.map((value) => (
           <Grid item spacing={3}>
             <Card className={classes.card}>
-              <CardContent>
-                <Typography variant="h6" component="h2">{value.title}</Typography>
-                <Typography>{value.body}</Typography>
-              </CardContent>
+              <Button className={classes.detailButton}>
+                <Link to="/detail">
+                  <CardContent>
+                    <Typography variant="h6" component="h2">{value.title}</Typography>
+                    <Typography>{value.body}</Typography>
+                  </CardContent>
+                </Link>
+              </Button>
             </Card>
           </Grid>
         )) }
