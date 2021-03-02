6ca6d358946383f10e71e8b70a1972176ddb8364
koukibuu3 <koukibuu3@gmail.com>
2021-03-02 20:39:56 +0900

    chore: eslintのReact用プラグイン追加 / eslintの設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
@@ -30,6 +32,11 @@ module.exports = {
         tabWidth: 2,
         trailingComma: 'es5',
       }
-    ]
+    ],
+    // ReactHooks に関する設定
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    // PropTypes を無効
+    'react/prop-types': 'off',
   },
 }
