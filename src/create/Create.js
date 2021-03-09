2f2a0348b034784f4e43490822ef6ceb0140a80c
t-rakuyama <t.rakuyama929@gmail.com>
2021-03-09 18:49:35 +0900

    入力枠をTextFieldにする

--- src/create/Create.js
+++ src/create/Create.js
@@ -3,7 +3,8 @@ import { useForm } from "react-hook-form";
 import Header from '../components/Header';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import { Grid, Typography } from '@material-ui/core';
+import { Box, Grid, TextField, Typography } from '@material-ui/core';
+
 
 
 
@@ -23,38 +24,42 @@ const Create = () => {
           </Grid>
           <form autoComplete="off" onSubmit={handleSubmit(onSubmit)} style={{ width: '100%' }}>
             <Grid item>
-              title
-              <input
-                type="text"
+              <TextField
                 label='title'
-                className={errors.title && 'error'}
+                margin="dense"
+                fullWidth
                 name="title"
-                ref={register({
-                  required: "titleは入力必須です",
-                  maxLength: { value: 30, message: 'titleは30文字以内で入力してください。' }
-                })}
+                helperText={errors.title && errors.title.message}
+                inputRef={
+                  register({
+                    required: "titleは入力必須です",
+                    maxLength: { value: 30, message: 'titleは30文字以内で入力してください。' }
+                  })
+                }
               />
-              {errors.title && errors.title.message}
             </Grid>
             <Grid item>
-              body
-              <input
-                type="text"
+              <TextField
                 label='body'
+                margin="dense"
+                fullWidth
                 name="body"
-                className={errors.body && 'error'}
-                ref={register({
-                  required: "bodyは入力必須です",
-                  minLength: { value: 10, message: 'bodyは10文字以上で入力してください。' }
-                })}
-                />
-                {errors.body && errors.body.message}
-            </Grid>
+                helperText={errors.body && errors.body.message}
+                inputRef={
+                  register({
+                    required: "bodyは入力必須です",
+                    minLength: { value: 10, message: 'bodyは10文字以上で入力してください。' }
+                  })
+                }
+              />
+              </Grid>
             <Button type="submit" variant="contained" color="primary">保存</Button>
           </form>
         </Grid>
       </Container>
-      <Link to='/'><Button type="button">一覧ページに戻る</Button></Link>
+      <Box my={2}>
+        <Link to='/'><Button type="button">一覧ページに戻る</Button></Link>
+      </Box>
     </>
   );
 };
