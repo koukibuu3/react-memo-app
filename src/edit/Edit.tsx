98dd84715ef2929c4b85c5e628f883393d41a106
koukibuu3 <koukibuu3@gmail.com>
2021-03-30 18:12:25 +0900

    feat: メモの更新処理を作成 #35

--- src/edit/Edit.tsx
+++ src/edit/Edit.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import Header from '../components/Header'
+import { Memo } from '../types'
+import { UpdateMemo as updateMemo } from '../utils'
 
 const Edit: React.FC = () => {
-  const contents = {
+  // TODO 今は固定でデータを用意しているため、編集対象のメモデータを渡してくるようにする
+  // TODO 特にidを固定で'1'に設定しているためどの記事を編集してもid='1'の記事が更新されます
+  const content: Memo = {
+    id: '1',
     title: 'たいとるだよー',
     body: 'ほんぶんだよー',
   }
+  const [memo, setMemo] = useState<Memo>(content)
 
   return (
     <>
@@ -17,18 +23,20 @@ const Edit: React.FC = () => {
         <tr>
           <th>タイトル</th>
           <td>
-            <textarea>{contents.title}</textarea>
+            <textarea onChange={(e) => setMemo({ ...memo, title: e.target.value })}>{memo.title}</textarea>
           </td>
         </tr>
         <tr>
           <th>本文</th>
           <td>
-            <textarea>{contents.body}</textarea>
+            <textarea onChange={(e) => setMemo({ ...memo, body: e.target.value })}>{memo.body}</textarea>
           </td>
         </tr>
       </table>
       <Link to="/detail">
-        <button type="button">保存</button>
+        <button type="button" onClick={() => updateMemo(memo)}>
+          保存
+        </button>
       </Link>
     </>
   )
