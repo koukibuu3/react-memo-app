31e487a4c8be6b286c612d1184ab1253c5810354
t-rakuyama <t.rakuyama929@gmail.com>
2021-05-13 08:27:37 +0900

    新規作成の処理を追加

--- src/create/Create.tsx
+++ src/create/Create.tsx
@@ -1,15 +1,24 @@
 import { Box, Grid, TextField, Typography } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 import Header from '../components/Header'
+import { Memo } from '../types'
+import { CreateMemo as createMemo } from '../utils'
 
 const Create = () => {
   const { register, handleSubmit, errors } = useForm()
-  const onSubmit = (value) => console.log(value)
+  const [memo, setMemo] = useState<Memo>()
+  const history = useHistory()
+
+  const onSubmit = (value) => {
+    console.log(value)
+    createMemo(memo)
+    history.push('/')
+  }
 
   return (
     <>
@@ -33,6 +42,7 @@ const Create = () => {
                   required: 'titleは入力必須です',
                   maxLength: { value: 30, message: 'titleは30文字以内で入力してください。' },
                 })}
+                onChange={(e) => setMemo({ ...memo, title: e.target.value })}
               />
             </Grid>
             <Grid item>
@@ -46,6 +56,7 @@ const Create = () => {
                   required: 'bodyは入力必須です',
                   minLength: { value: 10, message: 'bodyは10文字以上で入力してください。' },
                 })}
+                onChange={(e) => setMemo({ ...memo, body: e.target.value })}
               />
             </Grid>
             <Button type="submit" variant="contained" color="primary">
